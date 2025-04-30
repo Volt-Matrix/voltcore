4f26140fde15e77a1d90de8af11e130f246aa3b2
Chaitanya Sanivada <saichaitanyas@voltmatrix.net>
2025-04-30 13:10:05 +0530

    add clockinout component

--- src/components/ClockInOutBox/ClockInOut.jsx
+++ src/components/ClockInOutBox/ClockInOut.jsx
@@ -7,13 +7,14 @@ function ClockInOut () {
     // const [clockedInDuration, setClockedInDuration] = useState();
     const clockedInDuration = {"hours":4, "mins": 35}
     return (
-        <div className="clockinout_content-wrapper">
+        <div className="clockinout_content-wrapper card-p osns">
             <div className="clockinout_duration">
-                <p>{clockedInDuration['hours']} hours {clockedInDuration['mins']} mins</p>
-                <p>clocked-in duration</p>
+                <p><span className="clockinout_big-num">{clockedInDuration['hours']}</span> hours <span className="clockinout_big-num">{clockedInDuration['mins']}</span> mins</p>
+                <p className='clockinout_subtext color-grey'>clocked-in duration</p>
             </div>
             <div className="clockinout_buttons">
-
+                <button className="clockinout_button clockinout_clockin-button">Clock IN</button>
+                <button className="clockinout_button clockinout_clockout-button">Clock OUT</button>
             </div>
         </div>
     );
