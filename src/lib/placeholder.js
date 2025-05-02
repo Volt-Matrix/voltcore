3930891229dcb3e129165f1fc5f1d173a33b491a
Aasritha-Gopisetti <ashrithagopisetti@gmail.com>
2025-05-02 16:14:43 +0530

    Page:leave history and leave dashboard

---
+++ src/lib/placeholder.js
@@ -0,0 +1,47 @@
+export const availableHolidayList = [
+  { date: '2025-01-01', name: "New Year's Day" },
+  { date: '2025-02-14', name: "Valentine's Day" },
+  { date: '2025-03-17', name: "St. Patrick's Day" },
+  { date: '2025-04-20', name: 'Easter Sunday' },
+  { date: '2025-05-05', name: 'Cinco de Mayo' },
+  { date: '2025-07-04', name: 'Independence Day (USA)' },
+  { date: '2025-10-31', name: 'Halloween' },
+  { date: '2025-11-27', name: 'Thanksgiving (USA)' },
+  { date: '2025-12-25', name: 'Christmas Day' },
+  { date: '2025-12-31', name: "New Year's Eve" },
+]
+export const myDailyAttendance = [
+    {
+        date:'4/29/2025',
+        status:1,
+        checkIn:"9:00AM",
+        checkOut:"5:00PM",
+        totalHours:8
+    },
+    {
+        date:'4/27/2025',
+        status:1,
+        checkIn:"9:00AM",
+        checkOut:"5:00PM",
+        totalHours:8
+    }
+    , {
+        date:'4/2/2025',
+        status:1,
+        checkIn:"9:00AM",
+        checkOut:"5:00PM",
+        totalHours:8
+    }, {
+        date:'4/23/2025',
+        status:0,
+        checkIn:"",
+        checkOut:"",
+        totalHours:0
+    }, {
+        date:'4/25/2025',
+        status:1,
+        checkIn:"9:00AM",
+        checkOut:"5:00PM",
+        totalHours:8
+    }
+]
