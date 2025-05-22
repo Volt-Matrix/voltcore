a1f7fe2aa43d40d3a77e071faf4b0c5ee8a74f7e
Chaitanya Sanivada <saichaitanyas@voltmatrix.net>
2025-05-22 15:06:27 +0530

    add API integration for posting and fetching announcements

--- src/api/services.js
+++ src/api/services.js
@@ -1,3 +1,4 @@
+import { useResolvedPath } from 'react-router-dom';
 import api from './axiosConfig';
 import { loginUser } from './urls';
 export const registerUser = (postData) => {
@@ -8,5 +9,27 @@ export const registerUser = (postData) => {
 export const loginUserService = (postData) => {
   api.post('login/', postData).then((res) => {
     console.log(res.data);
+  });
+};
+
+export const postAnnouncement = (postData) => {
+  return api.post('announcements/', postData).then((resp) => {
+    console.log('post successfull: ', resp)
+    return resp;
+  })
+  .catch((error) => {
+    console.error("Post failed", error);
+    throw error;
+  });
+};
+
+export const getAnnouncements = () => {
+  return api.get('announcements/').then((resp) => {
+    console.log('fetched announcements: ', resp.data)
+    return resp.data;
+  })
+  .catch((error) => {
+    console.error('announcement get failed: ', error)
+    throw error;
   });
 };
