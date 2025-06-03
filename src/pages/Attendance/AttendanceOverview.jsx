f1fb1dc81a570f9f987585d6319c597c4c392c5c
Chaitanya Sanivada <saichaitanyas@voltmatrix.net>
2025-06-03 17:50:22 +0530

    add skeleton for attendance overview

---
+++ src/pages/Attendance/AttendanceOverview.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AttendanceOverview.css'
+
+function AttendanceOverview () {
+    return (
+        <div className="page-layout-container">
+            <p>Attendance overview placeholder</p>
+        </div>
+    )
+}
+
+export default AttendanceOverview;
